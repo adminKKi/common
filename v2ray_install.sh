ee4fed8992df82174ea190b87322ef3ae465ad00
WangYiKai <62984898+adminKKi@users.noreply.github.com>
2024-07-25 14:53:42 +0800

    Update v2ray_install.sh

--- v2ray_install.sh
+++ v2ray_install.sh
@@ -887,7 +887,7 @@ get_ip() {
 	[[ -z $ip ]] && ip=$(curl -s myip.ipip.net | grep -oE "([0-9]{1,3}\.){3}[0-9]{1,3}")
 	export "$(wget -4 -qO- https://dash.cloudflare.com/cdn-cgi/trace | grep ip=)" >/dev/null 2>&1
 	[[ -z $ip ]] && export "$(wget -6 -qO- https://dash.cloudflare.com/cdn-cgi/trace | grep ip=)" >/dev/null 2>&1
-	[[ -z $ip ]] && echo -e "\n$red 获取IP失败, 这垃圾小鸡扔了吧！$none\n" && exit
+	[[ -z $ip ]] && echo -e "\n$red 获取IP失败, 这不是原装的垃圾小鸡扔了吧！$none\n" && exit
 }
 
 error() {
